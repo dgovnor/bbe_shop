e4bb81823c28039876fbea1c0bb91c247bc1dd58
jude <jude@awellhealth.com>
2021-07-22 05:14:42 +0100

    configuration setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders grow above and beyond', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/grow above and beyond/i);
   expect(linkElement).toBeInTheDocument();
 });
